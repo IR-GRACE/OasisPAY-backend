3e88ad3402fd3e0c852e85ed69fb2c4ff854234e
JULVIER MUKENDI <stypojulvier009@mail.com>
2026-06-06 03:47:09 +0200

    Fix: ajout de relationship et Text dans les imports SQLAlchemy

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-﻿from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, Text, ForeignKey
+﻿from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, Text, ForeignKey\nfrom sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
